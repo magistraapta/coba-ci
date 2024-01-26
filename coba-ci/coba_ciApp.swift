db28fb734b41fc5f458f3b3aab2aca5a4088551b
magistraapta <magsitraagis@gmail.com>
2024-01-26 19:13:24 +0800

    Initial Commit

---
+++ coba-ci/coba_ciApp.swift
@@ -0,0 +1,17 @@
+//
+//  coba_ciApp.swift
+//  coba-ci
+//
+//  Created by Magistra Apta on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct coba_ciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
